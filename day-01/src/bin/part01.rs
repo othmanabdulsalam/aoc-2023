5afa88f893d950e7aa82a29fe4875161d5b006d1
Othman Abdulsalam <59613149+othmanabdulsalam@users.noreply.github.com.>
2024-01-07 21:00:59 +0000

    More changes to day 1

--- day-01/src/bin/part01.rs
+++ day-01/src/bin/part01.rs
@@ -1,18 +1,4 @@
-use std::fs::{self};
-
-#[derive(Clone, Copy)]
-enum CheckDigitType
-{
-    LeftMost,
-    RightMost
-}
-#[derive(Clone,Copy)]
-struct DigitStruct
-{
-    digit: u32,
-    position: usize
-}
-
+use std::fs;
 
 fn main() {
     // Read the input from the file 
@@ -27,81 +13,27 @@ fn main() {
         }
     }
 
-    // begin parsing file from left to right, finding right most then left mose file 
-    let mut total : u32 = 0;
-    let lines = text.lines();
-    
-    let mut current_check_num = DigitStruct {
-        digit : 0,
-        position : 0
-    };
 
+    let output = text 
+        .lines()
+        .map(|line| {
+            let mut it =
+                line.chars().filter_map(|character| {
+                    character.to_digit(10)
+                });
+            let first =
+                it.next().expect("should be a number");
 
-    let mut current_most_num = DigitStruct {
-        digit : 0,
-        position : 0
-    };
-
-    // start loop
-    for line in lines
-    {
-        let mut check_type = CheckDigitType::LeftMost;
-
-        for (pos, value) in line.char_indices()
-        {
-            //check the char is a number 
-            if value.is_digit(10)
-            {
-                current_check_num.digit = value.to_digit(10).unwrap();
-                current_check_num.position = pos;
-
-                if check_digit(current_check_num, current_most_num, check_type)
-                {
-                    //update current most to that value 
-                    current_most_num = current_check_num.clone();
-                    // first time it will be left most we are searching for, need to account for
-                    // that
-                    match check_type{
-                        CheckDigitType::LeftMost => {
-                            check_type = CheckDigitType::RightMost;
-                            total += current_most_num.digit * 10;
-                        }
-                        _ => ()
-                    }
-                }
+            match it.last() {
+                Some(num) => format!("{first}{num}"),
+                None => format!("{first}{first}"),
             }
-        }
-        total += current_most_num.digit;
-    }
-
-    println!("{}", total ); 
+            .parse::<u32>()
+            .expect("should be a valid number")
+        })
+        .sum::<u32>();
+        
+    println!("{}", output.to_string() ); 
     return;
 }
 
-
-
-
-
-/// This function checks if the digit is the left or right most that is being searched for,
-/// depending on what check type is passed in
-fn check_digit( current_check_digit: DigitStruct, 
-                current_most_digit:DigitStruct, check_type: CheckDigitType ) -> bool 
-{
-    match check_type {
-        CheckDigitType::LeftMost => return true,
-        CheckDigitType::RightMost => {
-            ()
-        }
-    };
-
-
-
-    if current_check_digit.position > current_most_digit.position
-    {
-        return true;
-    }
-    else {
-        return false;
-    }
-
-}
--- day-01/src/bin/part02.rs
+++ day-01/src/bin/part02.rs
@@ -1,3 +1,44 @@
+use std::fs;
+
 fn main() {
-    println!("Hello from part 2");
+    // Read the input from the file 
+    let file = fs::read_to_string("./src/bin/input.txt");
+
+    let text : String; 
+
+    match file {
+        Ok(res) => text = res,
+        Err(error) => { 
+             panic!("{:?}", error);
+        }
+    }
+
+
+    let output = text 
+        .lines()
+        .map(|line| {
+            parse_line(line)
+        })
+        .sum::<u32>();
+        
+    println!("{}", output.to_string() ); 
+    return;
+}
+
+
+fn parse_line( line: &str ) -> u32 
+{
+    let mut it =
+        line.chars().filter_map(|character| {
+            character.to_digit(10)
+        });
+    let first =
+        it.next().expect("should be a number");
+
+    match it.last() {
+        Some(num) => format!("{first}{num}"),
+        None => format!("{first}{first}"),
+    }
+    .parse::<u32>()
+        .expect("should be a valid number")
 }
